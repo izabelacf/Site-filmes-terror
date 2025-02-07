489e5ef429701d1b78d5f0002258b07def46da04
Izabela Cristina <izabelacfw@gmail.com>
2025-02-07 12:08:53 -0300

    up

--- js.js
+++ js.js
@@ -1,10 +1,9 @@
 document.getElementById("theme-toggle").addEventListener("click", function() {
-    const body = document.body;
-    
-    if (body.classList.contains("light-theme")) {
-      body.classList.replace("light-theme", "dark-theme");
-    } else {
-      body.classList.replace("dark-theme", "light-theme");
-    }
-  });
+  const body = document.body;
   
+  if (body.classList.contains("light-theme")) {
+      body.classList.replace("light-theme", "dark-theme");
+  } else {
+      body.classList.replace("dark-theme", "light-theme");
+  }
+});
